2fc3d0f96196701f976425fe76f3f07247d24a9a
samdelfino <54412724+samdelfino@users.noreply.github.com>
2022-02-15 19:22:20 -0500

    update click

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function button1Clicked() {
     let countClicks = 0;
     function upCount() { 
 
-        if(document.getElementById('circle1').innerHTML) {
+        if(document.getElementById('circle1')) {
             countClicks++;
             alert(countClicks);
             return true;
