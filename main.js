aecd1a7a4ae7fb8108ad6ae48f85ba34a75ede0d
samdelfino <54412724+samdelfino@users.noreply.github.com>
2022-02-16 16:53:01 -0500

    updating circle1

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-function button1Clicked() {
-    let countClicks = 0;
-    function upCount() { 
 
-        if(document.getElementById('circle1').onclick("button1Clicked()")) {
-            countClicks++;
-            alert(countClicks);
-            return true;
-        } else {
-            return false;
-        }
+function clickME() {
+    let click = document.getElementById("circle1-text").textContent; //getElementById() not getElementByID() Which you corrected in edit
+    let click_num = Number(click) + 1; 
+    document.getElementById("circle1-text") = String(click_num);
+ }
 
-    };
-}
+ const Circle1 = document.getElementById("circle1");
+
+ Circle1.addEventListener("click", clickME);
