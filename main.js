c80113d1118a5d9834a50c336785f7e1b2802f2f
samdelfino <54412724+samdelfino@users.noreply.github.com>
2022-02-16 16:58:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
 function clickME() {
-    let click = document.getElementById("circle1-text").textContent; //getElementById() not getElementByID() Which you corrected in edit
-    let click_num = Number(click) + 1; 
+    let click_circle_text = document.getElementById("circle1-text").textContent; //getElementById() not getElementByID() Which you corrected in edit
+    let click_num = Number(click_circle_text) + 1; 
     document.getElementById("circle1-text") = String(click_num);
  }
 
- const Circle1 = document.getElementById("circle1");
+ const circle1 = document.getElementById("circle1");
 
- Circle1.addEventListener("click", clickME);
+ circle1.addEventListener("click", clickME);
