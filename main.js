8a63b6a11adcc0884d75e588bbfa6420a5989a98
samdelfino <54412724+samdelfino@users.noreply.github.com>
2022-02-15 19:19:30 -0500

    add click count

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-file 
+function button1Clicked() {
+    let countClicks = 0;
+    function upCount() { 
+
+        if(document.getElementById('circle1').innerHTML) {
+            countClicks++;
+            alert(countClicks);
+            return true;
+        } else {
+            return false;
+        }
+
+    };
+}
